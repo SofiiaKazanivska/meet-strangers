5563c8079d37014cd4319d3f92b840b577ad6b6f
Sofiia <sofiakazzz@gmail.com>
2022-10-11 20:42:20 +0300

    The program displays hello + the name of the entered guest

--- src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
+++ src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
@@ -1,10 +1,31 @@
 package com.epam.rd.autotasks.meetstrangers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HelloStrangers {
     public static void main(String[] args) throws IOException {
-        //Write a program, asks for a number - amount of strangers to meet.
-        //Then reads stranger names line by line and prints line by line "Hello, ...".
+
+        Scanner x = new Scanner(System.in);
+        int count_strangers = x.nextInt();
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i <= count_strangers; i++) {
+            String s = x.nextLine();
+            list.add(s);
+        }
+
+        for (int i = 1; i < list.size(); i++) {
+            int n = list.size() - i - 1;
+            System.out.println("Hello, " + list.get(i));
+        }
+
+        if (count_strangers == 0) {
+            System.out.println("Oh, it looks like there is no one here");
+        }
+        if (count_strangers < 0) {
+            System.out.println("Seriously? Why so negative?");
+        }
     }
 }
